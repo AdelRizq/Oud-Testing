13925772981914dca9e9e37357435d057f996849
AdelRizq <adel.m.rizq@gmail.com>
2020-06-10 13:56:45 +0200

    move final work to master

--- Oud_TestingTeam(ANDROID)/src/test/java/BaseClass.java
+++ Oud_TestingTeam(ANDROID)/src/test/java/BaseClass.java
@@ -1,4 +1,4 @@
-
+package tests;
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
@@ -6,11 +6,8 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentTest;
-
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
@@ -20,10 +17,11 @@ import io.appium.java_client.touch.offset.PointOption;
 
 public class BaseClass extends extentReports {
 	DesiredCapabilities cap;
-	static AndroidDriver<MobileElement> driver;
-	String homeID = "com.spotify.music:id/home_tab";
+	AndroidDriver<MobileElement> driver;
+
 	@BeforeTest
-	public void setup() {
+	public void setup()
+	{
 		try {
 			setCaps();
 		} catch (Exception exp) {
@@ -33,40 +31,41 @@ public class BaseClass extends extentReports {
 		}
 	}
 
+	@AfterSuite
+	public void TearDown() {
+		tearDown();
+		driver.close();
+		driver.quit();
+	}
+
 	void setCaps() throws Exception {
 		cap = new DesiredCapabilities();
 
-		// this four capabilities for the used device
-		cap.setCapability("deviceName", "HUAWEI GR3 2017");
-		cap.setCapability("udid", "PSEDU17216005889");
+		cap.setCapability("deviceName", "Dola");
+		cap.setCapability("udid", "VWD7N17608000751");
 		cap.setCapability("platformName", "Android");
-		cap.setCapability("platformversion", "8.0.0");
-		//////////////////
+		cap.setCapability("platformVersion", "7.0");
 
 		cap.setCapability("appPackage", "com.spotify.music");
 		cap.setCapability("appActivity", "com.spotify.music.MainActivity");
 
 		URL url = new URL("http://localhost:4723/wd/hub");
 		driver = new AndroidDriver<MobileElement>(url, cap);
-		System.out.println("Testing Started");
 
-		while (getItemWebViewId(homeID).size() == 0){
-            System.out.println("Please login...");
-        }
+		System.out.println("Testing Started");
 	}
 
-	public MobileElement getElementById(String id) {
+	public MobileElement getElementById(String id)
+	{
 		return driver.findElementById(id);
 	}
 
-	public MobileElement getElementByXpath(String xpath) {
+	public MobileElement getElementByXpath(String xpath)
+	{
 		return driver.findElementByXPath(xpath);
 	}
-	public MobileElement getElementByText (String text)
-	{
-		return driver.findElement(By.xpath("//*[text()='"+text+"']"));
-	}
 
+	@SuppressWarnings("rawtypes")
 	public void Scroll(boolean direction) { // direction == 1 ? down : up
 		Dimension dimension = driver.manage().window().getSize();
 
@@ -76,9 +75,11 @@ public class BaseClass extends extentReports {
 		Double scrollHeightEnd = dimension.getHeight() * (direction ? 0.2 : 0.5);
 		int scrollEnd = scrollHeightEnd.intValue();
 
-		new TouchAction((PerformsTouchActions) driver).press(PointOption.point(0, scrollStart))
-				.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(2))).moveTo(PointOption.point(0, scrollEnd))
-				.release().perform();
+		new TouchAction((PerformsTouchActions) driver)
+		.press(PointOption.point(0, scrollStart))
+		.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(2)))
+		.moveTo(PointOption.point(0, scrollEnd))
+		.release().perform();
 	}
 
 	public List<MobileElement> getItemWebViewId(String id) {
@@ -90,21 +91,49 @@ public class BaseClass extends extentReports {
 	}
 
 	public MobileElement ScrollUntilId(String id, boolean direction, int idx) {
-		while (getItemWebViewId(id).size() == 0) {
+		while(getItemWebViewId(id).size() == 0)
+		{
 			Scroll(direction);
 		}
-		if (getItemWebViewId(id).size() > 0) {
+		if (getItemWebViewId(id).size() > 0)
+		{
 			return getItemWebViewId(id).get(idx);
 		}
 		return null;
 	}
 
+	public MobileElement ScrollUntilId(String id) {
+		while(getItemWebViewId(id).size() == 0)
+		{
+			Scroll(false);
+		}
+		if (getItemWebViewId(id).size() > 0)
+		{
+			return getItemWebViewId(id).get(0);
+		}
+		return null;
+	}
+
 	public MobileElement ScrollUntilXpath(String xpath, boolean direction, int idx) {
-		while (getItemWebViewXpath(xpath).size() == 0) {
+		while(getItemWebViewXpath(xpath).size() == 0)
+		{
 			Scroll(direction);
 		}
-		if (getItemWebViewXpath(xpath).size() > 0) {
+		if (getItemWebViewXpath(xpath).size() > 0)
+		{
 			return getItemWebViewXpath(xpath).get(idx);
+		}
+		return null;
+	}
+
+	public MobileElement ScrollUntilXpath(String xpath) {
+		while(getItemWebViewXpath(xpath).size() == 0)
+		{
+			Scroll(false);
+		}
+		if (getItemWebViewXpath(xpath).size() > 0)
+		{
+			return getItemWebViewXpath(xpath).get(0);
 		}
 		return null;
 	}
--- Oud_TestingTeam(ANDROID)/src/test/java/extentReports.java
+++ Oud_TestingTeam(ANDROID)/src/test/java/extentReports.java
@@ -1,5 +1,5 @@
+package tests;
 
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -8,9 +8,9 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 @SuppressWarnings("deprecation")
 public class extentReports {
 
-	ExtentHtmlReporter homeReporter, signupReporter, loginReporter, playerReporter,libraryArtistReporter,libraryAlbumReporter,libraryPlaylistReporter,searchReporter;
+	ExtentHtmlReporter homeReporter, signupReporter, loginReporter, playerReporter, playlistReporter, sharingReporter;
 
-	ExtentReports signup, home, login, Home_Account, PlayerRepo,libraryArtist,libraryAlbum,libraryPlaylist,search;
+	ExtentReports signup, home, login, Home_Account, PlayerRepo, playlist, sharing;
 
 	@BeforeSuite
 	public void reportSetup() {
@@ -18,53 +18,31 @@ public class extentReports {
 		homeReporter = new ExtentHtmlReporter("reports/Home_Account.html");
 		loginReporter = new ExtentHtmlReporter("reports/login.html");
 		playerReporter = new ExtentHtmlReporter("reports/Player.html");
+		playlistReporter = new ExtentHtmlReporter("reports/playlist.html");
+		sharingReporter = new ExtentHtmlReporter("reports/sharing.html");
 
-		libraryArtistReporter = new ExtentHtmlReporter("reports/libraryArtist.html");
-		libraryAlbumReporter= new ExtentHtmlReporter("reports/libraryAlbum.html");
-		libraryPlaylistReporter = new ExtentHtmlReporter("reports/libraryPlaylist.html");
-		searchReporter = new  ExtentHtmlReporter("reports/search.html");
-		
-
-
-		
 		Home_Account = new ExtentReports();
 		signup = new ExtentReports();
 		login = new ExtentReports();
 		PlayerRepo = new ExtentReports();
+		playlist = new ExtentReports();
+		sharing = new ExtentReports();
 
-		libraryAlbum    = new ExtentReports();
-		libraryArtist   = new ExtentReports();
-		libraryPlaylist = new ExtentReports();
-		search = new ExtentReports();
 		Home_Account.attachReporter(homeReporter);
 		PlayerRepo.attachReporter(playerReporter);
 		signup.attachReporter(signupReporter);
-
 		login.attachReporter(loginReporter);
-		libraryAlbum.attachReporter(libraryAlbumReporter); 
-		libraryArtist.attachReporter(libraryArtistReporter); 
-		libraryPlaylist.attachReporter(libraryPlaylistReporter); 
-		search.attachReporter(searchReporter);
+		playlist.attachReporter(playlistReporter);
+		sharing.attachReporter(sharingReporter);
 	}
 
-	@AfterSuite
-	public void artistFlush()
-	{
-		libraryArtist.flush();
-	}
-	@AfterSuite
-	public void playlistFlush()
-	{
-		libraryAlbum.flush();
-	}
-	@AfterSuite
-	public void albumFlush()
-	{
-		libraryPlaylist.flush();
-	}
-	@AfterSuite
-	public void searchFlush()
-	{
-		search.flush();
+	public void tearDown() {
+		// calling flush writes everything to the log file
+		Home_Account.flush();
+		PlayerRepo.flush();
+		login.flush();
+		signup.flush();
+		playlist.flush();
+		sharing.flush();
 	}
 }
